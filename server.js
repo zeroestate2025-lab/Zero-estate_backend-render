8fb3a8b02103fbb9ec589218d6ae5feb28622ddb
krishna25102002 <tripdaries01@gmail.com>
2025-10-17 00:02:22 +0530

    fixed of the land[only bed,bath,kitche showing]

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" })); // handles Base64 image uploads
+// app.use(express.json({ limit: "10mb" })); // handles Base64 image uploads
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 mongoose
   .connect(process.env.MONGO_URI)
