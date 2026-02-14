93b370d394a0a120759cde3b62da01c8f9c391d4
krishna25102002 <tripdaries01@gmail.com>
2026-02-14 14:29:32 +0530

    removed unwanted codes stuffs

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -9,10 +9,3 @@ const generateToken = (id) => {
 export default generateToken;
 
 
-// import jwt from "jsonwebtoken";
-
-// const generateToken = (id) => {
-//   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
-// };
-
-// export default generateToken;
